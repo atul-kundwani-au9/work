5c03a67d7e15da59996f7958fc54e1b3a7b86f65
atul-kundwani-au9 <67588922+atul-kundwani-au9@users.noreply.github.com>
2020-07-09 22:19:37 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+# Input a no from the user and print if its even or odd
+
+number=int(input("enter the number:" ))
+
+if number%2==0:
+    print(number,"number is even")
+else:
+    print(number,"number is odd")
+
+
+   
